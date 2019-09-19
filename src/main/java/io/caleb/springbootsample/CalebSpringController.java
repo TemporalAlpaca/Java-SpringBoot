f098ae0cee3c80e0b1a98aa53e61aa81e13e9c48
Caleb Kauffman <ckauffman16@gmail.com>
2019-09-18 20:00:52 -0700

    Added two simple endpoints to show functionality to retrieve data from request body and from visited URL.

--- src/main/java/io/caleb/springbootsample/CalebSpringController.java
+++ src/main/java/io/caleb/springbootsample/CalebSpringController.java
@@ -1,5 +1,8 @@
 package io.caleb.springbootsample;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,18 @@ public class CalebSpringController {
 	@RequestMapping("/HelloWorld")
 	public String helloWorld() {
 		return "Hello World!";
+	}
+	
+	@GetMapping("/SquareMyNumber/{number}")
+	public double squareMyNumber(@PathVariable double number){
+		if(number == 0)
+			return 0;
+		//This function will square the incoming double value and return it.
+		return Math.pow(number, 2);
+	}
+	
+	@GetMapping("/AddTen")
+	public double addTen(@RequestBody double number) {
+		return number + 10;
 	}
 }
